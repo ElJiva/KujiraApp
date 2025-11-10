cec4b6c360e0a00f02bdc34cdaea76c249be1d1c
ElJiva <angeljiva48@outlook.com>
2025-11-10 15:57:01 -0600

    KujiraApp Color Palette implementation

--- app/src/main/java/com/comics/kujiraapp/ui/theme/Color.kt
+++ app/src/main/java/com/comics/kujiraapp/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryBackground = Color(0xFF2C3E50)  // Dark Blue
+val PrimaryAccent = Color(0xFFE74C3C)       // Vibrant Red
+val SecondaryText = Color(0xFFECF0F1)       // Light Gray
+val HighlightColor = Color(0xFFF39C12)      // Orange
