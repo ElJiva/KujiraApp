4c40f0464b1067ef20b7458936bb8ee0beaeeb9e
Ramsestpz <tepozramses@gmail.com>
2025-11-11 17:19:15 -0600

    Fuentes

--- app/src/main/java/com/comics/kujiraapp/ui/theme/Type.kt
+++ app/src/main/java/com/comics/kujiraapp/ui/theme/Type.kt
@@ -1,34 +1,31 @@
-package com.comics.kujiraapp.ui.theme
+package com.rasec.musicapp.ui.theme
+
+
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.comics.kujiraapp.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
+val poppinsFontFamily = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_light, FontWeight.W300),
+    Font(R.font.poppins_bold, FontWeight.W700)
+)
+
+val poppinsTypo = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontWeight = FontWeight.Bold,
+        fontFamily = poppinsFontFamily,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    bodyMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontFamily = poppinsFontFamily,
+    ),
+    bodySmall = TextStyle(
+        fontWeight = FontWeight.Light,
+        fontFamily = poppinsFontFamily,
+    ),
 )
