6b13196d828b845c249355bed1d49304fd644772
josue4117 <jat79517@lasallebajio.edu.mx>
2025-11-29 16:39:25 -0600

    commit inicial de inicio de sesion y registro, agregue en la carpeta auth los archivos LoginScreen y SignUpScreenuna para el inicio de sesion y otra para el registro

---
+++ app/src/main/java/com/comics/kujiraapp/screens/auth/LoginScreen.kt
@@ -0,0 +1,15 @@
+package com.comics.kujiraapp.screens.auth
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginScreen() {
+    // TODO: Build the login screen UI
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
+}
