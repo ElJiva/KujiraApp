f4cd0b925ccd76d42b55d521d7cd0b2c8f822eff
josue4117 <jat79517@lasallebajio.edu.mx>
2025-11-29 17:19:55 -0600

    Añadir la función de registro a la interfaz de la API de autenticación

--- app/src/main/java/com/comics/kujiraapp/services/AuthApi.kt
+++ app/src/main/java/com/comics/kujiraapp/services/AuthApi.kt
@@ -2,10 +2,15 @@ package com.comics.kujiraapp.services
 
 import com.comics.kujiraapp.models.auth.LoginRequest
 import com.comics.kujiraapp.models.auth.LoginResponse
+import com.comics.kujiraapp.models.auth.SignUpRequest
+import com.comics.kujiraapp.models.auth.SignUpResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthApi {
     @POST("login")
     suspend fun login(@Body request: LoginRequest): LoginResponse
+
+    @POST("signup")
+    suspend fun signUp(@Body request: SignUpRequest): SignUpResponse
 }
