8ef5197655a1e4bb6f8e376855a50d8dbe88099d
josue4117 <jat79517@lasallebajio.edu.mx>
2025-11-29 16:41:02 -0600

    signupscreen creado

---
+++ app/src/main/java/com/comics/kujiraapp/screens/auth/SignUpScreen.kt
@@ -0,0 +1,15 @@
+package com.comics.kujiraapp.screens.auth
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SignUpScreen() {
+    // TODO: Build the sign-up screen UI
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignUpScreenPreview() {
+    SignUpScreen()
+}
